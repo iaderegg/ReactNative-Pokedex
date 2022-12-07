0610aa2f34e4b580874a3333c65a14ac8e189590
iaderegg <iadergg@gmail.com>
2022-12-07 17:47:46 -0500

    Create request to Poken API

--- screens/Pokedex.js
+++ screens/Pokedex.js
@@ -1,8 +1,24 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPokemonsApi } from '../src/api/pokemon';
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })()
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
