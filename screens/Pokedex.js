e4354058746b51dda7f0f357e84d3cd960e1a7ac
iaderegg <iadergg@gmail.com>
2022-12-09 19:14:58 -0500

    Loading more Pokemons

--- screens/Pokedex.js
+++ screens/Pokedex.js
@@ -6,6 +6,7 @@ import PokemonList from '../src/components/PokemonList';
 export default function Pokedex() {
 
   const [pokemons, setPokemons] = useState([]);
+  const [nextUrl, setnextUrl] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -15,7 +16,8 @@ export default function Pokedex() {
 
   const loadPokemons = async () => {
     try {
-      const response = await getPokemonsApi();
+      const response = await getPokemonsApi(nextUrl);
+      setnextUrl(response.next);
 
       const pokemonsArray = [];
 
@@ -40,7 +42,11 @@ export default function Pokedex() {
 
   return (
     <SafeAreaView>
-      <PokemonList pokemons={pokemons} />
+      <PokemonList
+        pokemons={pokemons}
+        loadPokemons={loadPokemons}
+        hasNext={nextUrl}
+      />
     </SafeAreaView>
   );
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 import PokemonCard from "./PokemonCard";
 
 export default function pokemonList(props) {
 
-  const { pokemons } = props;
+  const { pokemons, loadPokemons, hasNext } = props;
+
+  const loadMore = () => {
+    loadPokemons();
+  };
 
   return (
     <FlatList
@@ -14,6 +18,17 @@ export default function pokemonList(props) {
       keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
+      onEndReached={hasNext && loadMore}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={
+        hasNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
+        )
+      }
     />
   )
 }
@@ -21,5 +36,9 @@ export default function pokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5
+  },
+  spinner: {
+    marginTop: 20,
+    marginBottom: 60,
   },
 });
